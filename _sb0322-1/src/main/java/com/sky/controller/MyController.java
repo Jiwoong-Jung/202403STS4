41849c47ecbd8a22b18cc95c1a7c101c35076731
Jiwoong-Jung <jjw981119@naver.com>
2024-03-22 14:47:35 +0900

    오라클 연동

--- _sb0322-1/src/main/java/com/sky/controller/MyController.java
+++ _sb0322-1/src/main/java/com/sky/controller/MyController.java
@@ -1,13 +1,34 @@
 package com.sky.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sky.dto.Dept;
 
 @Controller
 public class MyController {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@GetMapping("/")
-	public String root() {
+	public String root(Model model) {
+		List<Dept> list = new ArrayList<>();
+		SqlRowSet rs = jdbcTemplate.queryForRowSet("select * from dept");
+		while(rs.next()) {
+			Dept dept = Dept.builder().deptno(rs.getInt("deptno"))
+					.dname(rs.getString("dname"))
+					.loc(rs.getString("loc")).build();
+			list.add(dept);
+		}
+		model.addAttribute("list", list);
 		return "root";
 	}
 }
--- _sb0322-1/src/main/java/com/sky/dto/Dept.java
+++ _sb0322-1/src/main/java/com/sky/dto/Dept.java
@@ -1,5 +1,6 @@
 package com.sky.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
