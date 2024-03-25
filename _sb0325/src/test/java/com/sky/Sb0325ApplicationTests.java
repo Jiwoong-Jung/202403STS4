e5217bdd2fbd7500cbb208fa9db00cfeb6f60865
Jiwoong-Jung <jjw981119@naver.com>
2024-03-25 10:53:17 +0900

    마이바티스 연습

--- _sb0325/src/test/java/com/sky/Sb0325ApplicationTests.java
+++ _sb0325/src/test/java/com/sky/Sb0325ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.sky;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sky.mapper.BoardMapper;
 
 @SpringBootTest
 class Sb0325ApplicationTests {
+	
+	@Autowired
+	BoardMapper boardMapper;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	@DisplayName("member 테이블의 개수")
+	void memberCount() {
+		System.out.println(boardMapper.selectCount());
 	}
 
 }
