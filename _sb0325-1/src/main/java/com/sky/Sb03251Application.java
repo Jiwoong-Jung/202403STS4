bd4000cc1af7469e12bbb2dd55e35e5ed94fb89b
Jiwoong-Jung <jjw981119@naver.com>
2024-03-26 14:45:45 +0900

    마이바티스 연습

---
+++ _sb0325-1/src/main/java/com/sky/Sb03251Application.java
@@ -0,0 +1,13 @@
+package com.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb03251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb03251Application.class, args);
+	}
+
+}
---
+++ _sb0325-1/src/test/java/com/sky/Sb03251ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb03251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
