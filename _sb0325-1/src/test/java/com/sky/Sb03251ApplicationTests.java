2c068f7493bfc6987c0972fe29c6de47c6569c59
Jiwoong-Jung <jjw981119@naver.com>
2024-03-26 14:54:54 +0900

    마이바티스 연습

--- _sb0325-1/src/test/java/com/sky/Sb03251ApplicationTests.java
+++ _sb0325-1/src/test/java/com/sky/Sb03251ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.sky;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sky.repository.MemberMapper;
 
 @SpringBootTest
 class Sb03251ApplicationTests {
+	
+	@Autowired
+	MemberMapper memberMapper;
 
 	@Test
 	void contextLoads() {
+		System.out.println(memberMapper.selectCount());
 	}
 
 }
