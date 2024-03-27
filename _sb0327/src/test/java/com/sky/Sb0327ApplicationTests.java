f2675cee0b7017eb4cf2ddfb21b9d48fd9d506f0
Jiwoong-Jung <jjw981119@naver.com>
2024-03-27 11:41:13 +0900

    h2 database

--- _sb0327/src/test/java/com/sky/Sb0327ApplicationTests.java
+++ _sb0327/src/test/java/com/sky/Sb0327ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.sky;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sky.repository.ProductMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class Sb0327ApplicationTests {
+	
+	@Autowired
+	ProductMapper productMapper;
 
 	@Test
 	void contextLoads() {
+		log.debug("products 레코드 수 {}", productMapper.count());
 	}
 
 }
