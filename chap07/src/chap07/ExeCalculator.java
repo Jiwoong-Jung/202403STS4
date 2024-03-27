4fb49f9201bc7a6948365958cee31f7db65b7559
Jiwoong-Jung <jjw981119@naver.com>
2024-03-27 14:34:19 +0900

    프록시 패턴

---
+++ chap07/src/chap07/ExeCalculator.java
@@ -0,0 +1,21 @@
+package chap07;
+
+public class ExeCalculator implements Calculator {
+
+	private Calculator delegate;
+	
+	public ExeCalculator(Calculator delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public long factorial(long num) {
+		long start = System.nanoTime();
+		long result = delegate.factorial(num);
+		long end = System.nanoTime();
+		System.out.printf("%s.fac(%d) 실행시간 = %d\n",
+				        delegate.getClass().getSimpleName(), (end-start));
+		return result;
+	}
+
+}
--- chap07/src/chap07/Main.java
+++ chap07/src/chap07/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ImpCalculator f1 = new ImpCalculator();
-		System.out.println(f1.factorial(5));
+		System.out.println(f1.factorial(20));
 		
 		RecCalculator f2 = new RecCalculator();
 		System.out.println(f2.factorial(5));
