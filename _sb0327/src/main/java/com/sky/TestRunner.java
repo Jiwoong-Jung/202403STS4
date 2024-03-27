d243b4dc75b3e63bf8a63f73cbdf8aa377983426
Jiwoong-Jung <jjw981119@naver.com>
2024-03-27 10:51:44 +0900

    h2 database

---
+++ _sb0327/src/main/java/com/sky/TestRunner.java
@@ -0,0 +1,34 @@
+package com.sky;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class TestRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Connection connection = dataSource.getConnection();
+		log.info("DBCP: " + dataSource.getClass()); // 사용하는 DBCP 타입 확인
+        log.info("Url: " + connection.getMetaData().getURL());
+        log.info("UserName: " + connection.getMetaData().getUserName());
+		
+	}
+
+}
