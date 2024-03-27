555d4bf20a74a7ba2d408916bcfb3fb01ff3949c
Jiwoong-Jung <jjw981119@naver.com>
2024-03-27 14:44:10 +0900

    프록시 패턴

--- chap07/src/chap07/MainProxy.java
+++ chap07/src/chap07/MainProxy.java
@@ -5,6 +5,9 @@ public class MainProxy {
 	public static void main(String[] args) {
 		ExeCalculator cal1 = new ExeCalculator(new ImpCalculator());
 		System.out.println(cal1.factorial(20));
+		
+		ExeCalculator cal2 = new ExeCalculator(new RecCalculator());
+		System.out.println(cal2.factorial(20));
 
 	}
 
