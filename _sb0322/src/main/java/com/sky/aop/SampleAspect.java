25114efc6e451b12406165ca34ed2d43e98bb4d1
Jiwoong-Jung <jjw981119@naver.com>
2024-03-28 14:54:45 +0900

    AOP 연습

--- _sb0322/src/main/java/com/sky/aop/SampleAspect.java
+++ _sb0322/src/main/java/com/sky/aop/SampleAspect.java
@@ -1,6 +1,10 @@
 package com.sky.aop;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,9 +12,17 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class SampleAspect {
-	@Before("execution(* com.sky.constroller.*.*(..))")
+	@Before("execution(* com.sky.controller.*.*(..))")
 	public void beforeAdvice(JoinPoint joinPoint) {
 		System.out.println("----------before----------");
 		System.out.println(joinPoint.getSignature().getName());
+	}
+	
+	@After("execution(* com.sky.dao.*.*(..))")
+	public void afterAdvice(JoinPoint joinPoint) {
+		System.out.println("----------after----------");
+		System.out.println(new SimpleDateFormat("yyyy/MM/dd")
+				                                    .format(new Date()));
+		System.out.println("메서드명: "+joinPoint.getSignature().getName());
 	}
 }
