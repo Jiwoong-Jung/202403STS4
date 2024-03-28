4783a0254ba044f62bf3b3e4db0771fd9a0d729f
Jiwoong-Jung <jjw981119@naver.com>
2024-03-28 11:29:18 +0900

    project 만들기

--- _sb0328/src/test/java/com/sky/Sb0328ApplicationTests.java
+++ _sb0328/src/test/java/com/sky/Sb0328ApplicationTests.java
@@ -2,12 +2,14 @@ package com.sky;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sky.model.Product;
+import com.sky.repository.MemberMapper;
 import com.sky.repository.ProductMapper;
 import com.sky.service.ProductService;
 
@@ -18,30 +20,39 @@ import lombok.extern.slf4j.Slf4j;
 class Sb0328ApplicationTests {
 
 	@Autowired
+	MemberMapper memberMapper;
+	
+	@Autowired
 	ProductMapper productMapper;
 	
 	@Autowired
     private ProductService productService;
 
 	@Test
+	@DisplayName("member 테이블 자료 건수")
+	void memberCount() {
+		log.info("member 수 {}", memberMapper.count());
+	}
+	
+	
 	void contextLoads() {
 		log.debug("products 레코드 수 {}", productMapper.count());
 	}
 	
-	@Test
+	//@Test
     public void getProductById() {
         Product product = productService.getProductById(1L);
         log.info("product : {}", product);
     }
 	
-	@Test
+	//@Test
     public void getAllProducts() {
         List<Product> products = productService.getAllProducts();
         log.info("products : {}", products);
     }
 
-    @Transactional
-    @Test
+    //@Transactional
+    //@Test
     public void addProduct() {
         productService.addProduct(new Product("쿤달 샴푸", 7900));
         productService.addProduct(new Product("마스크팩", 1000));
