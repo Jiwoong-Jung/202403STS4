4a4b54669f45d105e5b8133e93c2533c808417f5
Jiwoong-Jung <jjw981119@naver.com>
2024-03-29 10:43:36 +0900

    머스테치

--- _sb0329/src/main/java/com/sky/controller/MyController.java
+++ _sb0329/src/main/java/com/sky/controller/MyController.java
@@ -1,16 +1,25 @@
 package com.sky.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sky.repository.ProductMapper;
+import com.sky.service.ProductService;
+
 @Controller
 public class MyController {
+	
+	@Autowired
+	ProductService productService;
 	
 	@GetMapping("/")
 	public String main(Model model) {
 		model.addAttribute("userName", "홍길동");
 		model.addAttribute("aaa123", "가나다");
+		model.addAttribute("product", 
+				    productService.getProductById(3L));
 		return "main";
 	}
 }
