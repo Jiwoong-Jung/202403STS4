554fc340327651850250acb2584d8e85c08d527f
Jiwoong-Jung <jjw981119@naver.com>
2024-03-29 10:18:14 +0900

    머스테치

---
+++ _sb0329/src/main/java/com/sky/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.sky.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+}
