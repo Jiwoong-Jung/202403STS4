f22120634ecddabc5bb29e6e2e2752d869960b15
Jiwoong-Jung <jjw981119@naver.com>
2024-03-29 14:25:09 +0900

    개인 프로젝트 샘플

--- _sb0329/src/main/java/com/sky/entity/Orders.java
+++ _sb0329/src/main/java/com/sky/entity/Orders.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Order {
+public class Orders {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
