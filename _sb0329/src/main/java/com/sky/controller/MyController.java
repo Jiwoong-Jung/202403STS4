65cb474110cbb66c376de8924e524f9347904255
Jiwoong-Jung <jjw981119@naver.com>
2024-03-29 14:19:02 +0900

    머스테치

--- _sb0329/src/main/java/com/sky/controller/MyController.java
+++ _sb0329/src/main/java/com/sky/controller/MyController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sky.model.Product;
 import com.sky.repository.ProductMapper;
 import com.sky.service.ProductService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class MyController {
 	
@@ -16,22 +21,32 @@ public class MyController {
 	
 	@GetMapping("/")
 	public String main(Model model) {
-		model.addAttribute("userName", "홍길동");
-		model.addAttribute("aaa123", "가나다");
-		model.addAttribute("product", 
-				    productService.getProductById(3L));
-		return "main";
+//		model.addAttribute("userName", "홍길동");
+//		model.addAttribute("aaa123", "가나다");
+//		model.addAttribute("product", 
+//				    productService.getProductById(3L));
+		return "root";
+		// return "redirect:/list";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/items")
 	public String list(Model model) {
 		model.addAttribute("list", productService.getAllProducts());
 		return "list";
 	}
 	
-	@GetMapping("/save")
+	@GetMapping("/items/new")
 	public String save() {
 		return "save";
+	}
+	
+	@PostMapping("/new")
+	public String saveNew(Product product) {
+		log.info("--------------------------------------------");
+		log.info("product: {}", product.getProdName());
+		log.info("product: {}", product.getProdPrice());
+		productService.addProduct(product);
+		return "redirect:/items";
 	}
 	
 }
